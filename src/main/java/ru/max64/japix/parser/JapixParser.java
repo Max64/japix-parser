7b83fdabcf3b09172aaceb51d711f3797046c0d2
Max64 <max64.git@gmail.com>
2018-07-30 01:47:41 +0300

    init maven jar

---
+++ src/main/java/ru/max64/japix/parser/JapixParser.java
@@ -0,0 +1,12 @@
+package ru.max64.japix.parser;
+
+/**
+ * Created by forester7
+ */
+public class JapixParser {
+
+    public static void main(String[] args) {
+        System.out.println("Running");
+    }
+
+}
